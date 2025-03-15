91bd6fc3bf8838f7fe2689adf57162c7a630e4ce
Deepak1230987 <singhdeepak@kgpian.iitkgp.ac.in>
2025-03-15 21:37:35 +0530

    deployissue more

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,16 +1,18 @@
-import { Navigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function ProtectedRoute({ children }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [loading, setLoading] = useState(true);
 
+  const API_URL =
+    import.meta.env.VITE_API_URI?.trim() || "http://localhost:5000/api";
   useEffect(() => {
     const checkAuth = async () => {
       try {
-        await axios.get('http://localhost:5000/api/admin/coupons', {
-          withCredentials: true
+        await axios.get(API_URL, {
+          withCredentials: true,
         });
         setIsAuthenticated(true);
       } catch (error) {
